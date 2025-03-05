52613244f3c71621b34aa4244cc61f51a39b9aa6
ruminahugo <tinhyeuthuhanvasutradua@gmail.com>
2025-03-05 09:31:25 +0000

    Fix lỗi commit trong Codespaces

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 
 // Khởi tạo Scene, Camera, Renderer
---
+++ vite.config.js
@@ -0,0 +1,7 @@
+export default {
+    base: "./", // Tránh lỗi đường dẫn khi deploy
+    build: {
+      outDir: "dist", // Build ra thư mục `dist`
+    }
+  };
+  
