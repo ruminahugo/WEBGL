d68933b095a1a774ceab8d2d6ce356200ee2ed23
ruminahugo <tinhyeuthuhanvasutradua@gmail.com>
2025-03-07 06:08:20 +0000

    Fix lỗi commit trong Codespaces

--- main.js
+++ main.js
@@ -146,8 +146,7 @@ window.addEventListener('resize', () => {
 
 
 async function getDownSensors() {
-    const passhash = encodeURIComponent("Tan@0398017585"); 
-    const response = await fetch(`https://s16tc-prtg1-vp.vingroup.local/api/table.json?content=sensors&columns=objid,device,host,name,status&filter_status=5&username=Admin.tannm11&passhash=${passhash}`);
+    const response = await fetch("https://s16tc-prtg1-vp.vingroup.local/api/table.json?id=20694&content=sensors&columns=objid,device,host,name,status&filter_status=5&username=Admin.tannm11&passhash=Tan@0398017585");
     const data = await response.json();
     console.log(data);
 }
